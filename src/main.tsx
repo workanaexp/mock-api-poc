c2ad7436bda8d2a9719ad70d557b12a4dee148f7
Ana Experiments <anaexperiments@MacBook-Air-de-Ana.local>
2026-03-11 16:35:49 -0300

    remove flag

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,9 @@ import './index.css'
 import App from './App.tsx'
 
 async function enableMocking() {
-  if (import.meta.env.MODE !== 'development') {
-    return
-  }
+  // if (import.meta.env.MODE !== 'development') {
+  //   return
+  // }
 
   const { worker } = await import('./mocks/browser')
   await worker.start()
